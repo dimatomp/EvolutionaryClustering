43307758bd339da8e3172af0450473e3f7660f07
Dmitry Tomp <dmitrytomp@gmail.com>
2019-03-21 10:33:30 +0300

    Add csv reader

---
+++ analyze_csv.py
@@ -0,0 +1,24 @@
+import sys
+import pandas as pd
+from io import StringIO
+
+
+def read_results(fname):
+    with open(fname, 'r') as f:
+        content = f.read().split('\n')
+    assert content[-4].startswith('Running')
+    logs = StringIO('\n'.join(content[:-4]))
+    data = pd.read_csv(logs, index_col='generation')
+    success = data['index'][1:] != data['index'].data[:-1]
+    success = data.iloc[1:][success]
+    return data, success
+
+
+if __name__ == '__main__':
+    success = read_results(sys.argv[1])[1]
+    pd.set_option('display.max_rows', len(success))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', -1)
+    print(success)
