b985aa298362e1807476a58bb75da4a72bb79cb6
Dmitry Tomp <dmitrytomp@gmail.com>
2019-03-22 09:16:39 +0300

    Generate tables for indices and times

--- analyze_csv.py
+++ analyze_csv.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
+from batch_tasks import *
 from io import StringIO
+from itertools import chain
 
 
 def read_results(fname):
@@ -9,11 +11,45 @@ def read_results(fname):
     while content[-1] == '':
         content = content[:-1]
     assert content[-3].startswith('Running') or content[-3].startswith('Resulting')
+    running_time = None
+    for s in content[-3:]:
+        if s.startswith('Running time'):
+            running_time = float(s[13:s.find(' seconds')])
     logs = StringIO('\n'.join(content[:-3]))
     data = pd.read_csv(logs, index_col='generation')
     success = data['index'][1:] != data['index'].data[:-1]
     success = data.iloc[1:][success]
-    return data, success
+    return data, success, running_time
+
+
+def load_strategy_data(getter, prefix='.'):
+    columns = ['dataset'] + list(chain(*([index + '_trivial', index + '_dynamic'] for index, _ in indices)))
+    rows = []
+    for dataset, _ in datas:
+        row = [dataset]
+        for index, _ in indices:
+            try:
+                trivials = read_results(prefix + '/' + get_file_name(index, dataset, 'all_mutations_trivial'))
+                row.append(getter(trivials))
+            except AssertionError:
+                row.append(float('nan'))
+            try:
+                trivials = read_results(prefix + '/' + get_file_name(index, dataset, 'all_mutations_dynamic'))
+                row.append(getter(trivials))
+            except AssertionError:
+                row.append(float('nan'))
+        rows.append(row)
+    print(columns)
+    print(rows[0])
+    return pd.DataFrame(rows, columns=columns)
+
+
+def load_strategy_indices(prefix='.'):
+    return load_strategy_data(lambda trivials: trivials[1].iloc[-1]['index'], prefix=prefix)
+
+
+def load_strategy_times(prefix='.'):
+    return load_strategy_data(lambda x: x[2], prefix=prefix)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.metrics import adjusted_rand_score
 from sklearn.cluster import KMeans, AffinityPropagation, AgglomerativeClustering, MeanShift
 from time import time
 from multiprocessing import Pool
+from batch_tasks import tasks
 import traceback
 
 generated_prefix = '.'
@@ -68,71 +69,6 @@ def run_task(fname, index, datas, initialization, mutation, logging=None):
 
 
 if __name__ == "__main__":
-    datas = [
-        ('generated_2dim_10cl', 'load_generated_random_normal(2000, dim=2, n_clusters=10, prefix=generated_prefix)'),
-        ('generated_2dim_30cl', 'load_generated_random_normal(2000, dim=2, n_clusters=30, prefix=generated_prefix)'),
-        ('generated_10dim_10cl', 'load_generated_random_normal(2000, dim=10, n_clusters=10, prefix=generated_prefix)'),
-        ('generated_10dim_30cl', 'load_generated_random_normal(2000, dim=10, n_clusters=30, prefix=generated_prefix)'),
-        ('iris', 'normalize_data(load_iris(prefix=real_prefix))'),
-        ('immunotherapy', 'normalize_data(load_immunotherapy(prefix=real_prefix))'),
-        ('user_knowledge', 'normalize_data(load_user_knowledge(prefix=real_prefix))'),
-        ('mfeat_morphological', 'normalize_data(load_mfeat_morphological(prefix=real_prefix))'),
-        ('glass', 'normalize_data(load_glass(prefix=real_prefix))'),
-        ('haberman', 'normalize_data(load_haberman(prefix=real_prefix))'),
-        ('heart_statlog', 'normalize_data(load_heart_statlog(prefix=real_prefix))'),
-        ('vehicle', 'normalize_data(load_vehicle(prefix=real_prefix))'),
-        ('liver_disorders', 'normalize_data(load_liver_disorders(prefix=real_prefix))'),
-        ('oil_spill', 'normalize_data(load_oil_spill(prefix=real_prefix))')
-    ]
-    indices = [
-        ('silhouette', 'silhouette_index'),
-        ('calinski_harabaz', 'calinski_harabaz_index'),
-        ('davies_bouldin', 'davies_bouldin_index'),
-        ('dvcb_2', 'dvcb_index()'),
-        # ('dvcb_5', 'dvcb_index(d=5)'),
-        ('dunn', 'dunn_index'),
-        ('generalized_dunn_41', 'generalized_dunn_index(separation="centroid_distance", cohesion="diameter")'),
-        ('generalized_dunn_43', 'generalized_dunn_index(separation="centroid_distance", cohesion="mean_distance")'),
-        ('generalized_dunn_51', 'generalized_dunn_index(separation="mean_per_cluster", cohesion="diameter")'),
-        ('generalized_dunn_53', 'generalized_dunn_index(separation="mean_per_cluster", cohesion="mean_distance")'),
-        ('generalized_dunn_13', 'generalized_dunn_index(separation="single_linkage", cohesion="mean_distance")'),
-        # ('generalized_dunn_31', 'generalized_dunn_index(separation="mean_per_point", cohension="diameter")'),
-        # ('generalized_dunn_33', 'generalized_dunn_index(separation="mean_per_point", cohension="mean_distance")'),
-    ]
-    mutations = [
-        ## These mutations no longer exist
-        # ('centroid_hill_climbing', 'centroid_initialization', 'centroid_hill_climbing_mutation'),
-        # ('prototype_hill_climbing', 'prototype_initialization', 'prototype_hill_climbing_mutation'),
-        # ('knn_reclassification', 'axis_initialization', 'knn_reclassification_mutation'),
-        # ('one_nth_change', 'axis_initialization', 'one_nth_change_mutation')
-        ## These mutations are obsolete
-        # ('evocluster_diameter_centroid', 'axis_initialization',
-        #  'evo_cluster_mutation(diameter_separation, centroid_distance_cohesion)'),
-        # ('evocluster_mean_centroid', 'axis_initialization',
-        #  'evo_cluster_mutation(mean_centroid_distance_separation, centroid_distance_cohesion)'),
-        # ('evocluster_validity_centroid', 'axis_initialization',
-        #  'evo_cluster_mutation(density_based_validity_separation, centroid_distance_cohesion)'),
-        # ('evocluster_sparseness_centroid', 'axis_initialization',
-        #  'evo_cluster_mutation(density_based_sparseness_separation, centroid_distance_cohesion)'),
-        # ('evocluster_validity_separation', 'axis_initialization',
-        #  'evo_cluster_mutation(density_based_validity_separation, density_based_separation_cohesion)'),
-        # ('evocluster_sparseness_separation', 'axis_initialization',
-        #  'evo_cluster_mutation(density_based_sparseness_separation, density_based_separation_cohesion)'),
-        # ('split_eliminate', 'axis_initialization', 'split_eliminate_mutation'),
-        # ('split_merge_move', 'axis_initialization', 'split_merge_move_mutation'),
-        ('all_mutations_trivial', 'axis_initialization', 'all_moves_mutation(silent=True)'),
-        ('all_mutations_dynamic', 'axis_initialization', 'all_moves_dynamic_mutation(silent=True)')
-    ]
-    # tasks = [('state-of-the-art.txt', 'run_state_of_the_art([datas, indices])')]
-    tasks = []
-    for mutation_name, init, mutation in mutations:
-        for index_name, index in indices:
-            eval(index)
-            for data_name, data in datas:
-                fname = '{}-{}-{}.txt'.format(index_name, data_name, mutation_name)
-                tasks.append((fname,
-                              "run_task(output_prefix + '/' + '{}', {}, {}, {}, {})".format(fname, index, data, init,
-                                                                                            mutation)))
     if len(sys.argv) == 1:
         print('Total', len(tasks), 'tasks to run')
     elif len(sys.argv) == 2:
